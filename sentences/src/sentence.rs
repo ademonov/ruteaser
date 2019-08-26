cf190a4a17dc1d11a53270eadfde51c7bd4fe4cf
Andrew.Demonov <fcoder@live.com>
2019-08-25 23:31:59 -0400

    Improve sentences

--- sentences/src/sentence.rs
+++ sentences/src/sentence.rs
@@ -1,81 +1,75 @@
 use regex::Regex;
 use std::collections::BTreeSet;
 
-pub fn split_sentences<'a>(text: &'a str, delimiters: Vec<&str>, exceptions: Vec<&str>) -> Vec<&'a str> {
-    let mut breakpoints = BTreeSet::new();
-    for p in delimiters {
-        let p_len = p.chars().count();
-        for m in text.match_indices(p) {
-            for i in m.0..m.0 + p_len {
-                breakpoints.insert(i);
-            }
-        }
-    }
+//todo: create a struct for splitting rules
+pub fn split_sentences<'a>(text: &'a str, words_delimiters: Vec<&str>, regex_delimiters: Vec<&str>, words_exceptions: Vec<&str>, regex_exceptions: Vec<&str>) -> Vec<&'a str> {
+    let mut breakpoints = BTreeSet::new(); // contains all the positions we are going to split out text by.
 
-    for p in exceptions {
-        let p_len = p.chars().count();
-        for m in text.match_indices(p) {
-            println!("-{}", p);
-            for i in m.0..m.0 + p_len {
-                match breakpoints.remove(&i) {
-                    false => println!("--{}", i),
-                    true => println!("-+{}", i),
-                }
-            }
-            println!("_");
-        }
-    }
-
-    let r_exceptions = vec![r#"\d\.\d"#, r#"\s\.\d"#];
-
-    for p in r_exceptions {
-        let re = Regex::new(p).unwrap(); //todo: get rid of unwraps
-        println!("regex: {}", p);
-        for m in re.find_iter(text) {
-            println!("{}: {}..{}", m.as_str(), m.start(), m.end());
-        }
-    }
-
-
-
-
-    for i in breakpoints.iter() {
-        print!("{}, ", i)
-    }
-    println!("---");
+    dbg!(text);
+    handle_indices(text, words_delimiters, true, |x| { breakpoints.insert(x); });
+    dbg!(&breakpoints);
+    handle_indices(text, regex_delimiters, false, |x| { breakpoints.insert(x); });
+    dbg!(&breakpoints);
+    handle_indices(text, words_exceptions, true, |x| { breakpoints.remove(&x); });
+    dbg!(&breakpoints);
+    handle_indices(text, regex_exceptions, false, |x| { breakpoints.remove(&x); });
+    dbg!(&breakpoints);
 
     for i in 1..text.chars().count() {
         let current = i as usize;
         let previous = i - 1 as usize;
         if breakpoints.contains(&current) && breakpoints.contains(&previous) {
-            //todo: unimplemented
+            breakpoints.remove(&previous);
         }
     }
 
-    for (i, c) in text.chars().enumerate() {
-        let marker = if breakpoints.contains(&i) {
-            "*"
+    let mut result = vec![];
+    let mut cut = 0usize;
+    for i in breakpoints {
+        let next_cut = i + 1; // cut sentences by character right after the current delimiter
+        result.push(&text[cut..next_cut]);
+        cut = next_cut;
+    }
+    result.push(&text[cut..]); // add the latest sentence
+
+    result
+}
+
+fn handle_indices(text: &str, patterns: Vec<&str>, insensitive: bool, mut handle_index: impl FnMut(usize))
+{
+    for p in patterns {
+        let p = if insensitive {
+            String::from(p)
         } else {
-            " "
+            "(?i)".to_owned() + p
         };
 
-        println!("{} {}", c, marker);
+        let re = Regex::new(p.as_str()).unwrap(); //todo: get rid of unwraps
+        for m in re.find_iter(text) {
+            for i in m.start()..m.end() {
+                handle_index(i);
+            }
+        }
     }
-
-    let result = vec![];
-    result
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::sentence::split_sentences;
+//    use crate::sentence::split_sentences;
 
     #[test]
     fn it_works() {
-        let text = "Mr. Smith bought cheapsite.com for 1.5 million dollars, i.e. he paid a lot for it. Did he mind? Adam Jones Jr. thinks he didn't. In any case, this isn't true... Well, with a probability of .9 it isn't. C. elegans should do it. Ю";
-        for s in split_sentences(text) {
-            println!("{}", s);
-        }
+        let text = r"Maße, MASSE, mase, ΜΤΣ, abc, Abc, ABC, μτς";
+        let re = regex::Regex::new(r"(?i)μτς").unwrap();
+
+        let m: Vec<_> = re.find_iter(text).collect();
+        assert_eq!(m.len(), 2);
+
+
+//        let text = "Mr. Smith bought cheapsite.com for 1.5 million dollars, i.e. he paid a lot for it. Did he mind? Adam Jones Jr. thinks he didn't. In any case, this isn't true... Well, with a probability of .9 it isn't. C. elegans should do it. Ю";
+//        for s in split_sentences(text) {
+//            println!("{}", s);
+//        }
         //assert_eq!(s.chars().count(), 10);
     }
 }
